d72a16522c902f136fdb0c7e0e390aae167129bd
icyhot12 <rmierniczek1@gmail.com>
2022-09-29 19:11:29 +0200

    navbar logic - separate file

---
+++ navbar.js
@@ -0,0 +1,23 @@
+// navbar logic 
+const toggleBtn = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelector('.links');
+
+toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('show-links');
+
+    if (links.classList.contains('show-links-direction')) {
+        setTimeout(() => {
+            links.classList.remove('show-links-direction');
+        }, 1000);
+    } else {
+        links.classList.add('show-links-direction')
+    }
+});
+
+window.addEventListener('resize', (e) => {
+    if (window.innerWidth > 1225) {
+        navLinks.classList.remove('show-links')
+        links.classList.remove('show-links-direction')
+    }
+})
